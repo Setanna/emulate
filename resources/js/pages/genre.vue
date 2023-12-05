96ea299f417608a353eb255668d4176b9cf4bf20
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-12-05 12:09:45 +0100

    Updated: genres route and component now use name and id, genre component now watches for id change

--- resources/js/pages/genre.vue
+++ resources/js/pages/genre.vue
@@ -1,7 +1,8 @@
 <!-- genre.vue -->
 <template>
     <div class="genre">
-        <h1> {{ genre }} </h1>
+        <h1 v-if="genre"> {{ genre.name }} </h1>
+        <p v-if="genre"> {{ genre.description }}</p>
     </div>
 </template>
 
@@ -9,16 +10,31 @@
 import axios from 'axios';
 
 export default {
-    data () {
+    data() {
         return {
+            /* variables */
             genre: null
         }
     },
     // is necessary when changing to the same route but with different parameters
     watch: {
-        '$route.params.id' (to) {
-            this.genre = to;
+        '$route.params.id': {
+            handler(id) {
+                console.log(id)
+                // fetch new channel content when a query param is changed.
+                if(id !== undefined){
+                    axios.get('/api/genre/' + id).then(response => {
+                        this.genre = response.data
+                    })
+                        .catch(error => {
+                            console.log("Watch error: " + error)
+                        })
+                }
+            },
+            immediate: true,
+            deep: true
         }
     },
 }
 </script>
+
--- resources/js/router.js
+++ resources/js/router.js
@@ -10,7 +10,7 @@ const router = createRouter({
         },
         {
             name: 'genres',
-            path: '/:id',
+            path: '/:name',
             component: () => import('./pages/genre.vue')
         },
         {
