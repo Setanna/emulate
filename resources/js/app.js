239b34fd09304bbc95f834c3cedc06bcfc7f0ae0
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-12-05 10:19:56 +0100

    Removed: unused multiselect plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,10 @@ import { createApp } from "vue";
 import App from './layouts/app.vue';
 import router from './router';
 import axios from 'axios'
-import Multiselect from '@vueform/multiselect';
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.use(router);
-app.use(Multiselect);
 app.mount("#app");
 
 document.title = 'Emulate'
