62ab6f3391812646f8692d21def7562be9dc9658
Setanna <joenhendrikhoelckhansen@gmail.com>
2024-01-19 13:03:49 +0100

    Added: new books and talents to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -68,6 +68,8 @@ class DatabaseSeeder extends Seeder
         }
         Book("Core Rulebook", "The core rulebook for a generic fantasy system", 1,"2023/11/10");
         Book("The Draconic Expansion", "A book for a generic fantasy system filled with draconic options", 1,"2023/11/10");
+        Book("The Infernal Realms", "A book for a generic fantasy system filled with fiendish options and devil deals", 1,"2023/11/10");
+        Book("The Empyrean Realms", "A book for a generic fantasy system filled with angelic options and divine boons", 1,"2023/11/10");
         Book("Core Rulebook", "The core rulebook for a generic sci-fi system", 2, "2023/11/10");
         Book("Core Rulebook", "The core rulebook for a generic apocalypse system", 3, "2023/11/10");
         Book("Core Rulebook", "The core rulebook for a generic superhero system", 4, "2023/11/10");
@@ -84,9 +86,12 @@ class DatabaseSeeder extends Seeder
         Talent("Armor Training", 1, "You gain training in light armor", "You become trained with light armor.", 1);
         Talent("Weapon Training", 1, "You gain training in simple weapons", "You become trained with simple weapons.", 1);
         Talent("Rallying Charge", 1, "You rally your allies and charge forward", "You spend 3 actions to make a call to arms. You and all allies who can hear you can move up to their speed and make a single attack.", 1);
+        Talent("Dragon's Blood", 1, "You have the blood of a dragon coursing through you.", "system", 2);
         Talent("Dragon's Breath", 1, "You breathe the elements like the dragons of old", "system", 2);
         Talent("Draconic Scales", 1, "Scales that protect from both the elements and physical blows", "system", 2);
-        Talent("Xenophobic", -1, "You're xenophobic'", "system", 3);
+        Talent("Devil Contract", 0, "You make a contract with a Devil for fiendish powers.", "system", 3);
+        Talent("Divine Boon", 0, "Your actions have not gone unnoticed, and the heaven's above grant you their blessing.", "system", 4);
+        Talent("Xenophobic", -1, "You're xenophobic'", "system", 5);
 
         function RequiredTalent($talent_id, $required_talent_id){
             $Model = new \App\Models\RequiredTalent();
@@ -94,9 +99,8 @@ class DatabaseSeeder extends Seeder
             $Model->required_talent_id = $required_talent_id;
             $Model->save();
         }
-        RequiredTalent(2,1);
-        RequiredTalent(4,3);
-        RequiredTalent(6,5);
+        RequiredTalent(5, 4);
+        RequiredTalent(6,4);
 
         function Requirement($name, $description){
             $Model = new \App\Models\Requirement();
@@ -104,8 +108,7 @@ class DatabaseSeeder extends Seeder
             $Model->description = $description;
             $Model->save();
         }
-        Requirement("test1", "lorem ipsum");
-        Requirement("speech", "lorem ipsum");
+        Requirement("Speech", "description");
 
         function TalentRequirement($talent_id, $requirement_id){
             $Model = new \App\Models\TalentRequirement();
@@ -113,10 +116,7 @@ class DatabaseSeeder extends Seeder
             $Model->requirement_id = $requirement_id;
             $Model->save();
         }
-        TalentRequirement(1,1);
-        TalentRequirement(2,1);
-        TalentRequirement(5, 2);
-        TalentRequirement(6, 2);
+        TalentRequirement(3,1);
 
         function Category($name, $description, $system){
             $Model = new \App\Models\Category();
@@ -140,14 +140,17 @@ class DatabaseSeeder extends Seeder
             $Model->category_id = $category_id;
             $Model->save();
         }
-        TalentCategory(2,1);
         TalentCategory(1,2);
-        TalentCategory(5, 1);
-        TalentCategory(5, 3);
-        TalentCategory(5, 5);
-        TalentCategory(6, 1);
-        TalentCategory(6, 3);
-        TalentCategory(6, 5);
+        TalentCategory(2,1);
+        TalentCategory(3,1);
+        TalentCategory(3,3);
+        TalentCategory(3,5);
+        TalentCategory(4,2);
+        TalentCategory(5,1);
+        TalentCategory(6,2);
+        TalentCategory(7,5);
+        TalentCategory(8,5);
+        TalentCategory(9,8);
 
         function TraitModel($name, $description, $system){
             $Model = new \App\Models\TraitModel();
@@ -167,7 +170,9 @@ class DatabaseSeeder extends Seeder
             $Model->trait_id = $trait_id;
             $Model->save();
         }
-        TalentTrait(6,3);
+        TalentTrait(3,3);
+        TalentTrait(7,4);
+        TalentTrait(8,4);
 
         function Rule($name, $text, $book_id){
             $Model = new \App\Models\Rule();
